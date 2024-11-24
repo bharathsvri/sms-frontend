f02aba29bebf6f15ada2414ff25c0021a6750648
Bharath <143115318+bharathsvri@users.noreply.github.com>
2024-11-24 21:32:24 +0530

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import './Form.css'
-import { sendTextMessage } from "../../assets/data";
+import './Form.css';
 import axios from "axios";
 
 const Form = () => {
